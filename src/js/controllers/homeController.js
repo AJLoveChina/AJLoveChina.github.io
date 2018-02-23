5e09c098c6f25cc1f987c0e980f567f1ae5931af
AJLoveChina <735267452@qq.com>
2018-02-23 14:12:03 +0800

    作品

--- src/js/controllers/homeController.js
+++ src/js/controllers/homeController.js
@@ -2,15 +2,61 @@
  * Created by hejie2.0
  * Coding love on 2017/3/18.
  */
-hejie.controller("home", function ($scope, $rootScope, commonService) {
-    $scope.title = commonService.config.title;
-    $rootScope.$broadcast("head.changeTitle", "Home");
-});
+hejie.controller('home', function ($scope, $rootScope, commonService) {
+  $scope.title = commonService.config.title
+  $rootScope.$broadcast('head.changeTitle', 'Home')
+})
 
-hejie.controller("headCtrl", function ($scope, commonService) {
-    $scope.title = commonService.config.title;
+hejie.controller('headCtrl', function ($scope, commonService) {
+  $scope.title = commonService.config.title
 
-    $scope.$on("head.changeTitle", function (ev, title) {
-        $scope.title = title;
-    })
-});
+  $scope.$on('head.changeTitle', function (ev, title) {
+    $scope.title = title
+  })
+})
+
+hejie.controller('artsCtrl', function ($scope, commonService) {
+  $scope.title = '下面的列表是我创作的一些可展示的在线作品(其中标题有链接标识的网页程序可以在线演示)'
+
+  var arr = [
+    {
+      title: '图像处理(均衡化与二值化)',
+      link : "http://nigeerhuo.com/DigitalImageProcessing/index.html",
+      createTime: '2014-05-06',
+      git : "https://github.com/AJLoveChina/DigitalImageProcessing",
+      desc : "大学计算机图像处理课程期末作业, 使用JS实现图像处理相关的算法, 主要是大津阈值法"
+    }, {
+      title: '2048小游戏网页版',
+      link : "http://nigeerhuo.com/2048/index.html",
+      git : "https://github.com/AJLoveChina/2048",
+      createTime: '2015-07-18',
+      desc : "使用JS编写的2048小游戏, 使用Div+span, 非Canvas实现"
+    }, {
+      title: '宋词自动生成网页版',
+      link : "http://nigeerhuo.com/songci/index.html",
+      git : "https://github.com/AJLoveChina/Songci",
+      createTime: '2015-10-21',
+      desc : "大学自然语言处理课程期末作业, 主要是从宋词库中提取常用的词语根据词牌的押韵以及组合方式生成新的宋词"
+    }, {
+      title: '扫雷小游戏网页版',
+      git : "https://github.com/AJLoveChina/CleanBombs",
+      link : "http://nigeerhuo.com/CleanBombs/index.html",
+      createTime: '2017-01-26',
+      desc : "实现了和windows经典扫雷游戏一样的处理逻辑, 主要使用递归检查策略, 但是UI体验不太好 囧.."
+    }, {
+      title: '经典汉诺塔问题解法动态演示',
+      link : "http://nigeerhuo.com/Hanoi/index.html",
+      git : "https://github.com/AJLoveChina/Hanoi",
+      createTime: '2017-02-23',
+      desc : "在网页上动态展示如何解决汉诺塔问题, 使用JS编写"
+    }, {
+      title: '合肥高新区公寓空房自动查询申请工具',
+      git : "https://github.com/AJLoveChina/HefeiGaoxinApartment",
+      createTime: '2017-10-22',
+      desc : "自动登录, 自动查询空房, 查询到空房后自动申请"
+    }
+  ];
+  $scope.list = arr.reverse()
+
+})
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,127 +1,129 @@
-var hejie = angular.module("hejie", [ 'ui.router', 'oc.lazyLoad', 'ngSanitize']);
+var hejie = angular.module('hejie', ['ui.router', 'oc.lazyLoad', 'ngSanitize'])
 
 hejie.value('loadingService', {
-    dom : null,
-    request: function(config) {
-        var commonService = angular.element(document.body).injector().get('commonService');
-        commonService.showWaitingShadow();
-    },
-    response: function(response) {
-        var commonService = angular.element(document.body).injector().get('commonService');
-        commonService.hideWaitingShadow();
-    }
-});
+  dom: null,
+  request: function (config) {
+    var commonService = angular.element(document.body).injector().get('commonService')
+    commonService.showWaitingShadow()
+  },
+  response: function (response) {
+    var commonService = angular.element(document.body).injector().get('commonService')
+    commonService.hideWaitingShadow()
+  }
+})
 
-hejie.factory('loadingInterceptor', function($q, loadingService, $location, $timeout) {
-    return {
-        request: function(config) {
-            loadingService.request(config);
+hejie.factory('loadingInterceptor', function ($q, loadingService, $location, $timeout) {
+  return {
+    request: function (config) {
+      loadingService.request(config)
 
-            try {
-                var isAdmin = new URI(location.href).query(true).isAdmin;
-                if (isAdmin) {
-                    config.url = new URI(config.url).addSearch({isAdmin:isAdmin}).toString();
-                }
-            } catch (ex) {}
-            if (config.method.toUpperCase() === "GET" && !config.cache) {
-                // 解决在IE低版本浏览器会有缓存的问题
-                config.url = new URI(config.url).addSearch({"_date" : moment(new Date()).format("YYYYMMDD")}).toString()
-            }
-
-            return config;
-        },
-        response: function(response) {
-            loadingService.response(response);
-            return response;
+      try {
+        var isAdmin = new URI(location.href).query(true).isAdmin
+        if (isAdmin) {
+          config.url = new URI(config.url).addSearch({isAdmin: isAdmin}).toString()
         }
+      } catch (ex) {}
+      if (config.method.toUpperCase() === 'GET' && !config.cache) {
+        // 解决在IE低版本浏览器会有缓存的问题
+        config.url = new URI(config.url).addSearch({'_date': moment(new Date()).format('YYYYMMDD')}).toString()
+      }
+
+      return config
+    },
+    response: function (response) {
+      loadingService.response(response)
+      return response
     }
-});
+  }
+})
 
-hejie.config(function($stateProvider, $locationProvider, $urlRouterProvider, $logProvider, $httpProvider, $provide) {
-    $provide.decorator('$rootScope', ['$delegate', function($delegate){
+hejie.config(function ($stateProvider, $locationProvider, $urlRouterProvider, $logProvider, $httpProvider, $provide) {
+  $provide.decorator('$rootScope', ['$delegate', function ($delegate) {
 
-        Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
-            value: function(name, listener){
-                var unsubscribe = $delegate.$on(name, listener);
-                this.$on('$destroy', unsubscribe);
+    Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
+      value: function (name, listener) {
+        var unsubscribe = $delegate.$on(name, listener)
+        this.$on('$destroy', unsubscribe)
 
-                return unsubscribe;
-            },
-            enumerable: false
-        });
+        return unsubscribe
+      },
+      enumerable: false
+    })
 
+    return $delegate
+  }])
 
-        return $delegate;
-    }]);
+  $httpProvider.interceptors.push('loadingInterceptor')
 
-    $httpProvider.interceptors.push('loadingInterceptor');
+  $logProvider.debugEnabled(true)
+  $urlRouterProvider.when('', '/home')
 
-    $logProvider.debugEnabled(true);
-    $urlRouterProvider.when("", "/home");
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'src/tpl/home.html'
+    })
+    .state('homePage', {
+      url: '/home/page/:page',
+      templateUrl: 'src/tpl/home.html'
+    })
+    .state('me', {
+      url: '/me',
+      templateUrl: 'src/tpl/me.html'
+    })
+    .state('arts', {
+      url: '/arts',
+      templateUrl: 'src/tpl/arts.html'
+    })
+    .state('message', {
+      url: '/message',
+      templateUrl: 'src/tpl/message.html'
+    })
+    .state('test', {
+      url: '/test',
+      templateUrl: 'src/tpl/test.html'
+    })
+    .state('test.slick', {
+      url: '/slick',
+      templateUrl: 'src/tpl/test/slick.html'
+    })
+    .state('test.encodeXDecode', {
+      url: '/encodeXDecode',
+      templateUrl: 'src/tpl/test/encodeXdecode.html'
+    })
+    .state('test.questionnaire', {
+      url: '/questionnaire',
+      templateUrl: 'src/tpl/test/questionnaire.html'
+    })
+    .state('test.qqconnect', {
+      url: '/qqconnect',
+      templateUrl: 'src/tpl/test/qqconnect.html'
+    })
+    //
+    // .state('editor', {
+    //     url : '/editor',
+    //     templateUrl : "src/tpl/editor/editor.html"
+    // })
+    .state('test.ueditor', {
+      url: '/ueditor',
+      templateUrl: 'src/tpl/editor/ueditor.html'
+    })
+    .state('test.quill', {
+      url: '/quill',
+      templateUrl: 'src/tpl/editor/quill.html'
+    })
 
-    $stateProvider
-        .state('home', {
-            url: '/home',
-            templateUrl: 'src/tpl/home.html'
-        })
-        .state('homePage', {
-            url: '/home/page/:page',
-            templateUrl: 'src/tpl/home.html'
-        })
-        .state('me', {
-            url: '/me',
-            templateUrl: 'src/tpl/me.html'
-        })
-        .state('message', {
-            url: '/message',
-            templateUrl: 'src/tpl/message.html'
-        })
-        .state('test', {
-            url : '/test',
-            templateUrl : "src/tpl/test.html"
-        })
-        .state('test.slick', {
-            url : '/slick',
-            templateUrl : "src/tpl/test/slick.html"
-        })
-        .state('test.encodeXDecode', {
-            url : '/encodeXDecode',
-            templateUrl : "src/tpl/test/encodeXdecode.html"
-        })
-        .state('test.questionnaire', {
-            url : '/questionnaire',
-            templateUrl : "src/tpl/test/questionnaire.html"
-        })
-        .state('test.qqconnect', {
-            url : '/qqconnect',
-            templateUrl : "src/tpl/test/qqconnect.html"
-        })
-        //
-        // .state('editor', {
-        //     url : '/editor',
-        //     templateUrl : "src/tpl/editor/editor.html"
-        // })
-        .state('test.ueditor', {
-            url : '/ueditor',
-            templateUrl : "src/tpl/editor/ueditor.html"
-        })
-        .state('test.quill', {
-            url : '/quill',
-            templateUrl : "src/tpl/editor/quill.html"
-        })
+    .state('519', {
+      url: '/519',
+      templateUrl: 'src/tpl/519.html'
+    })
+    .state('blog', {
+      url: '/blog/:fileName',
+      templateUrl: 'src/tpl/blog.html'
+    })
+    .state('wenjuan', {
+      url: '/wenjuan/:fileName',
+      templateUrl: 'src/tpl/wenjuan.html'
+    })
 
-        .state('519', {
-            url : '/519',
-            templateUrl : "src/tpl/519.html"
-        })
-        .state('blog', {
-            url : '/blog/:fileName',
-            templateUrl : "src/tpl/blog.html"
-        })
-        .state('wenjuan', {
-            url : '/wenjuan/:fileName',
-            templateUrl : "src/tpl/wenjuan.html"
-        })
-
-
-});
+})
